f02ef561db2c8edefb8ea46b4d3744ad36c263c0
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-04-16 17:03:14 +0300

    oops broke

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("890403817:AAEg-CKr7K06KE4YUg4YwfIh3PZw0w4-v_w")
+    updater = Updater("719806814:AAGYz51c18jP2nLRBobWx1gpq8KauLmGJio")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
